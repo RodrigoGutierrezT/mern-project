88a3c3fde332ad0e3b0b7ab2bca93fb51119635a
Rodrigo Gutierrez <rodrigo644@gmail.com>
2022-03-01 11:48:47 -0300

    Create goalRoutes with mockup endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log('Hello World');
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// Mount Routes
+app.use('/api/goals', require('./routes/goalRoutes'));
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+})
