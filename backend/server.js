ec65df2cc0576eaee9cd30e32ecbe7a8290a21ea
Rodrigo Gutierrez <rodrigo644@gmail.com>
2022-03-01 22:59:20 -0300

    Add errorHandler middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const { errorHandler } = require('./middleware/errorMiddleware');
 const port = process.env.PORT || 5000;
 
 
@@ -10,6 +11,11 @@ app.use(express.urlencoded({extended: false}));
 
 // Mount Routes
 app.use('/api/goals', require('./routes/goalRoutes'));
+
+
+
+// Custom error handler
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
