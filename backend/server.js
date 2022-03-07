a76b81038bbce0410a83c9d4b86bab1a78e65c55
Rodrigo Gutierrez <rodrigo644@gmail.com>
2022-03-06 23:00:18 -0300

    Add user Routes with mock controllers

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(express.urlencoded({extended: false}));
 
 // Mount Routes
 app.use('/api/goals', require('./routes/goalRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
 
 
 // Custom error handler
