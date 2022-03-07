730e665f420fb616d7cc38e9707e20ee95e8a110
Rodrigo Gutierrez <rodrigo644@gmail.com>
2022-03-06 22:40:25 -0300

    Add userModel.js

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const goalSchema = new mongoose.Schema({
     text: {
         type: String,
-        required: [true, 'Please add a text value'],
-
+        required: [true, 'Please add a text value']
     }
 }, {
     timestamps: true
